379feeac08dfe44b31a7f6eb8683d8289c46faf3
philiptkd <philiptkd1@gmail.com>
2017-11-03 21:35:26 +0000

    gave unique IDs to created remote entities

--- python_scripts/hello_world.py
+++ python_scripts/hello_world.py
@@ -1,12 +1,24 @@
+#TODO
+#use numbered naming convention for remote entities
+#increment the largest group name number, use to create unique entity id
+#give unique entity id to new remote entity
+
+#get list of group entity IDs
+group_id_list = hass.states.entity_ids(domain_filter='group')
+
+#get first unused remote# entity ID
+i = 0
+while('group.remote'+str(i) in group_id_list):
+    i = i + 1
+entity_id = 'remote'+str(i)
 
 value = data.get('value')
-
 #creates entity on button press
 #hass.states.set('group.panel3', value)
 
 #create remote(script.dummy_panel) entity on button press
 panelAttributes = {'entity_id': ('script.dummy_panel',), 'view': 'yes'}
-hass.states.set('group.'+value,'hello', attributes=panelAttributes)
+hass.states.set('group.'+entity_id,'hello', attributes=panelAttributes)
 
 logger.info("Executing the {0} script.".format(value))
 hass.bus.fire(value, { "wow": "from a Python script!" })
--- python_scripts/print.py
+++ python_scripts/print.py
@@ -1,3 +1,6 @@
-state = hass.states.get('group.panel')
-attributes = state.attributes
-logger.info(attributes)
+#state = hass.states.get('group.panel')
+#attributes = state.attributes
+#logger.info(attributes)
+
+entities = hass.states.entity_ids(domain_filter='group')
+logger.info(entities)
