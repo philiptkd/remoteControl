9760dfcf32f9d69c871aaf00d4e94e191dee3e90
Philip Raeisghasem <philiptkd1@gmail.com>
2017-11-03 20:32:18 +0000

    buttons now create new remote entities in new views

--- custom_components/hello_state.py
+++ custom_components/hello_state.py
@@ -1,9 +1,10 @@
 DOMAIN = 'hello_state'
 
-panelAttributes = {'assumed_state': False, 'entity_id': ('script.dummy_panel',), 'friendly_name': 'Button Panel', 'order': 0}
+#panelAttributes = {'assumed_state': False, 'entity_id': ('script.dummy_panel',), 'friendly_name': 'Button Panel', 'order': 0}
 
+#creates extra remote entity on startup
 def setup(hass,config):
-   hass.states.set('group.world','hello', attributes=panelAttributes)
+#   hass.states.set('group.world','hello', attributes=panelAttributes)
    return True
 
 
--- python_scripts/hello_world.py
+++ python_scripts/hello_world.py
@@ -5,7 +5,7 @@ value = data.get('value')
 #hass.states.set('group.panel3', value)
 
 #create remote(script.dummy_panel) entity on button press
-panelAttributes = {'assumed_state': False, 'entity_id': ('script.dummy_panel',), 'friendly_name': 'Button Panel', 'order': 0}
+panelAttributes = {'entity_id': ('script.dummy_panel',), 'view': 'yes'}
 hass.states.set('group.'+value,'hello', attributes=panelAttributes)
 
 logger.info("Executing the {0} script.".format(value))
