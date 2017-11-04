535471fe2ace2bb1a0167da813e005787bdd64cb
philiptkd <philiptkd1@gmail.com>
2017-11-04 18:09:56 +0000

    added capability to name new remote devices

--- python_scripts/hello_world.py
+++ python_scripts/hello_world.py
@@ -1,25 +1,36 @@
+value = data.get('value')
+
+#get input text for new entity id
+entity_id = hass.states.get('input_text.add_device_name').state
+entity_id = entity_id.lower()
+
 #get list of group entity IDs
 group_id_list = hass.states.entity_ids(domain_filter='group')
 
-#get first unused remote# entity ID
-i = 0
-while('group.remote'+str(i) in group_id_list):
-    i = i + 1
-entity_id = 'remote'+str(i)
+#if the input text was empty
+if(entity_id == ''):
+    #get first unused remote# entity ID
+    i = 0
+    while('group.remote'+str(i) in group_id_list):
+        i = i + 1
+    entity_id = 'remote'+str(i)
 
-#get argument passed
-value = data.get('value')
+logger.info(group_id_list)
+logger.info(entity_id)
 
-#create remote(script.dummy_panel) entity on button press
-panelAttributes = {'entity_id': ('script.dummy_panel',), 'view': 'yes'}
-hass.states.set('group.'+entity_id,'hello', attributes=panelAttributes)
+#if there is not already a device with this name
+if('group.' + entity_id not in group_id_list):
+    #create remote(script.dummy_panel) entity on button press
+    panelAttributes = {'entity_id': ('script.dummy_panel',), 'view': 'yes'}
+    hass.states.set('group.'+entity_id,'hello', attributes=panelAttributes)
 
-#add option input select for deleting devices
-state = hass.states.get('input_select.delete_device_select')
-attributes = state.attributes
-attributes['options'].append(entity_id)
+    #add option input select for deleting devices
+    state = hass.states.get('input_select.delete_device_select')
+    attributes = state.attributes
+    attributes['options'].append(entity_id)
 
-hass.states.set('input_select.delete_device_select', state.state, attributes=attributes, force_update=True)
+    #set the new state of the input select
+    hass.states.set('input_select.delete_device_select', state.state, attributes=attributes, force_update=True)
 
 #print to logger
 logger.info("Executing the {0} script.".format(value))
