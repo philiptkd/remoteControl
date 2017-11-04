bfcf268604d2b6b01dea6d7cc9a88a5d4f3d8e08
philiptkd <philiptkd1@gmail.com>
2017-11-04 03:56:11 +0000

    jk, there's an argument to states.set that does the same thing

--- python_scripts/hello_world.py
+++ python_scripts/hello_world.py
@@ -23,11 +23,7 @@ state = hass.states.get('input_select.delete_device')
 attributes = state.attributes
 attributes['options'].append(entity_id)
 
-newState = 'Select Device to Delete'
-if(state.state == newState):
-  newState = attributes['options'][1]
-
-hass.states.set('input_select.delete_device', newState, attributes=attributes)
+hass.states.set('input_select.delete_device', state.state, attributes=attributes, force_update=True)
 
 #print to logger
 logger.info("Executing the {0} script.".format(value))
