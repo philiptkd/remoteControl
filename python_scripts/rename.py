38c2003f60f144e0e72da00c858712436aedd41d
philiptkd <philiptkd1@gmail.com>
2017-11-05 01:29:11 +0000

    let each 'add signal' button pass their unique name as their argument to their script

--- python_scripts/rename.py
+++ python_scripts/rename.py
@@ -1,5 +1,6 @@
 entity_id = data.get('entity_id')
 view_name = data.get('view_name')
+newName = entity_id+'_'+view_name
 
 #get state
 state = hass.states.get(entity_id)
@@ -21,10 +22,27 @@ if(entity_id[:6] == 'group.'):
         if(key != 'entity_id'):
             newAttributes[key] = attributes[key]
     newAttributes['entity_id'] = membersList
+#else if entity is a script, change the data it passes as an argument
+elif(entity_id[:7] == 'script.'):
+    newAttributes = {}
+    newAttributes['custom_ui_state_card'] = attributes['custom_ui_state_card']
+    configDict = {}
+    for configKey in attributes['config'].keys():
+        if(configKey != 'entities'):
+            configDict[configKey] = attributes['config'][configKey]
+    oldEntitiesList = attributes['config']['entities']
+    newEntitiesList = []
+    for i in range(0,len(oldEntitiesList)):
+        newEntitiesList.append({})
+        for entKey in oldEntitiesList[i].keys():
+            if(entKey != 'data'):
+                newEntitiesList[i][entKey] = oldEntitiesList[i][entKey]
+        newEntitiesList[i]['data'] = {'value': newName}
+    configDict['entities'] = newEntitiesList
+    newAttributes['config'] = configDict
 else:
     newAttributes = attributes
 
 #create new entity with edited name/attributes
-newName = entity_id+'_'+view_name
 hass.states.set(newName, stateStr, attributes=newAttributes)
 
